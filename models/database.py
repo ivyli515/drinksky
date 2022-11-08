f5c12dbc372b8a500fdd9bef9a73823e798edf1d
ivy <li.ivy5@gmail.com>
2022-11-08 20:40:20 +1100

    WIP

--- models/database.py
+++ models/database.py
@@ -5,18 +5,18 @@ from contextlib2 import closing
 DB_URL = os.environ.get('DATABASE_URL', 'dbname=drinksky')
 
 def sql_select_all(query, parameters):
-    with closing(psycopg2.connect('dbname = drinksky')) as conn, conn.cursor() as cur:
+    with closing(psycopg2.connect(DB_URL)) as conn, conn.cursor() as cur:
         cur.execute(query,parameters)
         result = cur.fetchall()
         return result
 
 def sql_select_one(query, parameters):
-    with closing(psycopg2.connect('dbname = drinksky')) as conn, conn.cursor() as cur:
+    with closing(psycopg2.connect(DB_URL)) as conn, conn.cursor() as cur:
         cur.execute(query,parameters)
         result = cur.fetchone()
         return result
 
 def sql_write(query, parameters):
-    with closing(psycopg2.connect('dbname = drinksky')) as conn, conn.cursor() as cur:
+    with closing(psycopg2.connect(DB_URL)) as conn, conn.cursor() as cur:
         cur.execute(query,parameters)
         conn.commit()
