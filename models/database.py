fff54944f1b0b8c1a4a385392c2ed5f6a83eab00
ivy <li.ivy5@gmail.com>
2022-11-08 20:33:53 +1100

    fix database

--- models/database.py
+++ models/database.py
@@ -1,5 +1,8 @@
+import os
 import psycopg2
 from contextlib2 import closing
+
+DB_URL = os.environ.get('DATABASE_URL', 'dbname=drinksky')
 
 def sql_select_all(query, parameters):
     with closing(psycopg2.connect('dbname = drinksky')) as conn, conn.cursor() as cur:
